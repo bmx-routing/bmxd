d5033f8c9163be7abdb0674f4834fc9b2de94bac
Marek Lindner <lindner_marek@yahoo.de>
2007-01-15 13:48:01 +0100

    delete custom files

--- allocate.h
+++
@@ -1,5 +0,0 @@
-void checkIntegrity(void);
-void checkLeak(void);
-void *debugMalloc(unsigned int length, int tag);
-void *debugRealloc(void *memory, unsigned int length, int tag);
-void debugFree(void *memoryParameter, int tag);
